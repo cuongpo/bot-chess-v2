ca45422ce21a087af2d58a382a0de8f2a3e4e700
cuongpham <61429471+cuongpo@users.noreply.github.com>
2022-11-05 11:20:50 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,6 @@
+// get next move
+document.querySelector("#chessvision-wrapper > div").shadowRoot.querySelector("#top > div > div.jss19.dropdown.btn-group > button:nth-child(1)").click();
+document.querySelector("#main-wrap > main > div.analyse__tools > div.pv_box > div").getAttribute("data-uci");
+
+// click 
+console.log(document.getElementsByClassName("bg-cover bg-center")[1].getAttribute("style"));

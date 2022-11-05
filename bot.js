9fe46f6ab97163749e8476ef910e15a1f2d4764b
cuongquep <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>
2022-11-05 12:34:12 +0700

    move2

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var next_move = document.querySelector("#main-wrap > main > div.analyse__tools >
 console.log(next_move);
 var text1 = ((104-next_move.charCodeAt(0))*100).toString() + "%," + (parseInt(next_move.slice(1,2))*100).toString() + "%)";
 console.log(text1);
-var text2 = ((104-next_move.charCodeAt(2))*100).toString() + "%," + (parseInt(next_move.slice(3,4))*100).toString() + "%)";
+var text2 = "--un-translate-x:"+ ((104-next_move.charCodeAt(2))*100).toString() + "%; --un-translate-y:" + (parseInt(next_move.slice(3,4))*100).toString() + "%)";
 console.log(text2);
 
 
@@ -16,74 +16,36 @@ var move1 = document.getElementsByClassName("transition-transform ease-in-out wi
 for (let i=0;i<move1.length;i++) {
 	if (move1[i].getAttribute("style").includes(text1)) {
 		console.log(move1[i].getAttribute("style"));
+		move1[i].click();
 		break;
 	}
 }
 
 
 // Click Text 2
-
-
-
-
-
-
-
-
-
-
-var move1 = document.getElementsByClassName("transition-transform ease-in-out will-change-transform");
-for (let i=0;i<move1.length;i++) {
-	console.log(move1[i].getAttribute("style"));
+var move2 = document.getElementsByClassName("transform pointer-events-none bg-black");
+for (let i=0;i<move2.length;i++) {
+	if (move2[i].getAttribute("style").includes(text2)) {
+		console.log(move2[i].getAttribute("style"));
+		move2[i].click();
+		break;
+	}
 }
 
-var move1 = document.getElementsByClassName("bg-cover bg-center");
-for (let i=1;i<=move1.length;i++) {
-	if (move1[i].getAttribute("style").includes(text1)) {
-		console.log(move1[i].getAttribute("style"));
+
+// <div class="absolute top-0 left-0 z-[1] w-1/8 h-1/8 transform pointer-events-none bg-black opacity-20 rounded-full scale-25" style="--un-translate-x:600%; --un-translate-y:500%;"></div>
+
+
+
+
+var move2 = document.getElementsByClassName("transform pointer-events-none bg-black");
+for (let i=0;i<move2.length;i++) {
+	if (move2[i].getAttribute("style").includes(text2)) {
+		console.log(move2[i].getAttribute("style"));
+		move2[i].click();
 		break;
 	}
 }
 
 
 
-var move1 = document.getElementsByClassName("bg-cover bg-center");
-for (let i=1;i<=move1.length;i++) {
-		console.log(move1[i].getAttribute("style"));
-
-}
-
-
-
-var move1 = document.getElementsByClassName("transition-transform ease-in-out will-change-transform");
-for (let i=0;i<move1.length;i++) {
-	if (move1[i].getAttribute("style").includes(text1)) {
-		console.log(move1[i].getAttribute("style"));
-		break;
-	}
-}
-
-
-103 
-console.log(text1)
-
-
-
-
-BLACK
-
-
-g -> 103 -> 1   
-a -> 97 -> 7
-
-
-z-index:50;transform:translate(700%, 0%)
-
-
-
-
-
-
-
-// click 
-console.log(document.getElementsByClassName("bg-cover bg-center")[1].getAttribute("style"));
